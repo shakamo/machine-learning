94cd8f9e6b4e688fc07daf6005cc956f7cc4ed68
Junichi Sakamoto <sakamoto.jun05@gmail.com>
2018-04-12 00:42:52 +0900

    Fix.

--- lib/prepare/feature_extraction.py
+++ lib/prepare/feature_extraction.py
@@ -76,7 +76,10 @@ def extract_for_fx_by_m1_vectorize(df):
     df['1_3_M1_lower_beard'] = df['4_close'] - df['3_low']
     df['1_4_M1_trend'] = df['1_open'] - df['4_close']
 
-    func_range_vectorize = np.vectorize(X_vectorize, excluded=[1, 2, 3])
+    func_range_vectorize = np.vectorize(X_range_vectorize, excluded=[1, 2, 3])
+    func_upper_beard_vectorize = np.vectorize(X_upper_beard_vectorize, excluded=[1, 2, 3])
+    func_lower_beard_vectorize = np.vectorize(X_lower_beard_vectorize, excluded=[1, 2, 3])
+    func_trend_vectorize = np.vectorize(X_trend_vectorize, excluded=[1, 2, 3])
     func_y_vectorize = np.vectorize(y_vectorize, excluded=[1, 2])
 
     df['1_11_M2_range'] = func_y_vectorize(df.index, df['2_high'], df['3_low'], 2)
@@ -88,50 +91,76 @@ def extract_for_fx_by_m1_vectorize(df):
     df['1_17_M240_range'] = func_y_vectorize(df.index, df['2_high'], df['3_low'], 240)
 
     df['2_1_M2_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 2)
-    df['2_2_M2_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 2)
-    df['2_3_M2_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 2)
-    df['2_4_M2_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 2)
+    df['2_2_M2_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 2)
+    df['2_3_M2_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 2)
+    df['2_4_M2_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 2)
 
     df['3_1_M3_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 3)
-    df['3_2_M3_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 3)
-    df['3_3_M3_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 3)
-    df['3_4_M3_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 3)
+    df['3_2_M3_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 3)
+    df['3_3_M3_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 3)
+    df['3_4_M3_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 3)
 
     df['5_1_M5_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 5)
-    df['5_2_M5_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 5)
-    df['5_3_M5_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 5)
-    df['5_4_M5_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 5)
+    df['5_2_M5_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 5)
+    df['5_3_M5_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 5)
+    df['5_4_M5_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 5)
 
     df['15_1_M15_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 15)
-    df['15_2_M15_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 15)
-    df['15_3_M15_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 15)
-    df['15_4_M15_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 15)
+    df['15_2_M15_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 15)
+    df['15_3_M15_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 15)
+    df['15_4_M15_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 15)
 
     df['30_1_M30_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 30)
-    df['30_2_M30_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 30)
-    df['30_3_M30_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 30)
-    df['30_4_M30_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 30)
+    df['30_2_M30_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 30)
+    df['30_3_M30_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 30)
+    df['30_4_M30_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 30)
 
     df['60_1_M60_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 60)
-    df['60_2_M60_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 60)
-    df['60_3_M60_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 60)
-    df['60_4_M60_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 60)
+    df['60_2_M60_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 60)
+    df['60_3_M60_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 60)
+    df['60_4_M60_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 60)
 
     df['240_1_M240_range'] = func_range_vectorize(df.index, df['2_high'], df['3_low'], 240)
-    df['240_2_M240_upper_beard'] = func_range_vectorize(df.index, df['2_high'], df['1_open'], 240)
-    df['240_3_M240_lower_beard'] = func_range_vectorize(df.index, df['4_close'], df['3_low'], 240)
-    df['240_4_M240_trend'] = func_range_vectorize(df.index, df['1_open'], df['4_close'], 240)
+    df['240_2_M240_upper_beard'] = func_upper_beard_vectorize(df.index, df['2_high'], df['1_open'], 240)
+    df['240_3_M240_lower_beard'] = func_lower_beard_vectorize(df.index, df['4_close'], df['3_low'], 240)
+    df['240_4_M240_trend'] = func_trend_vectorize(df.index, df['1_open'], df['4_close'], 240)
 
     return df
 
 
-def X_vectorize(index, x, y, m_size):
+def X_range_vectorize(index, x, y, m_size):
+    # high low
     a = x.ix[index - m_size + 1:index].dropna()
     b = y.ix[index - m_size + 1:index].dropna()
     return a.max() - b.min()
 
 
+def X_upper_beard_vectorize(index, x, y, m_size):
+    # high open
+    if 0 <= index - m_size + 1:
+        return 0
+    a = x.ix[index - m_size + 1:index].dropna()
+    b = y.ix[index - m_size + 1] # Error
+    return a.max() - b
+
+
+def X_lower_beard_vectorize(index, x, y, m_size):
+    # close low
+    a = x.ix[index]
+    b = y.ix[index - m_size + 1:index].dropna()
+    return a - b.min()
+
+
+def X_trend_vectorize(index, x, y, m_size):
+    # open close
+    if 0 <= index - m_size + 1:
+        return 0
+    a = x.ix[index - m_size + 1] # Error
+    b = y.ix[index]
+    return a - b
+
+
 def y_vectorize(index, x, y, m_size):
     a = x.ix[index:index + m_size - 1].dropna()
     b = y.ix[index:index + m_size - 1].dropna()
-    return (a.mean() + b.mean())/2
+    return a.mean()
